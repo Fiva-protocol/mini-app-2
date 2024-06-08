b34e28cd1ab722a23e6d9611737f4623a61d4953
elchin.rahimli <elchinr@maestro.st>
2024-06-08 21:22:06 +0200

    for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,44 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import path from 'path';
+
+
+const HOST = "0.0.0.0"
+const REPLACEMENT = `${path.resolve(__dirname, './src')}/`
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+  
+    
+  ],
+  base: "./",
+  server: {
+    host: process.env.HOST,
+    port: process.env.PORT,
+  },
+  //   server: {
+  //     port: process.env.PORT || 4000
+  //   },
   resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  }
-})
+    alias: [
+      {
+        find: "@/",
+        replacement: REPLACEMENT,
+      },
+      {
+        find: "src/",
+        replacement: REPLACEMENT,
+      },
+      {
+        find: /* ~/ */ /^~(?=\/)/,
+        replacement: path.join(__dirname, "node_modules"),
+      },
+      {
+        find: /* ~ */ /^~(?!\/)/,
+        replacement: path.join(__dirname, "node_modules/"),
+      },
+    ],
+    extensions: [".vue", ".js", ".jsx", ".ts", ".tsx", ".json"],
+  },
+});
